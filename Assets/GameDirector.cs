81096dedb759f9cfe3acf1ab29a4487bd30fb281
watanabe@tiffa <watanabe.everystudio@gmail.com>
2021-06-16 12:24:48 +0900

    プロジェクト追加

---
+++ Assets/GameDirector.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameDirector : MonoBehaviour
+{
+    [SerializeField]
+    private Healthbar m_healthbar;
+    private float m_fHealth;
+
+    void Start()
+    {
+        m_fHealth = 1.0f;
+        InvokeRepeating("DecreaseHealth", 0.0f, 0.1f);
+    }
+    private void DecreaseHealth()
+    {
+        m_fHealth -= 0.01f;
+        if( m_fHealth < 0.0f)
+        {
+            m_fHealth = 0.0f;
+        }
+        m_healthbar.SetSize(m_fHealth);
+    }
+
+
+
+}
---
+++ Assets/Healthbar.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Healthbar : MonoBehaviour
+{
+    private Transform m_tfBar;
+
+    private void Awake()
+    {
+        m_tfBar = transform.Find("bar");
+    }
+
+    public void SetSize(float _fNormalize)
+    {
+        m_tfBar.localScale = new Vector3(_fNormalize, 1.0f);
+        GetComponent<Animator>().SetFloat("health_rate", _fNormalize);
+    }
+}
